aed3580e7a4ae1d2cf4a199b6118a077e0e1a158
Elliot Whitehead <elliot.whitehead@lasp.colorado.edu>
2019-11-17 15:38:08 -0700

    Implement basic pixel painting!!!

--- js/pimage.js
+++ js/pimage.js
@@ -1,12 +1,32 @@
+let img;
+
+function preload() {
+    image_width = windowWidth - 40;
+    image_height = windowHeight - 40;
+    img = loadImage("https://picsum.photos/" + image_width + "/" + image_height);
+}
+
 function setup() {
-    createCanvas(640, 480);
-  }
-  
-  function draw() {
-    if (mouseIsPressed) {
-        fill(0);
-        ellipse(mouseX, mouseY, 80, 80);
-    } else {
-        noFill();
+    createCanvas(image_width, image_height*2);
+    background(255);
+    noStroke();
+    image(img, 0, 0);
+}
+
+function draw(){
+    if(mouseIsPressed){
+
+        if( mouseY <  image_height){
+            // get color (and opposite color) from photo selection
+            let palette = get(mouseX,mouseY);
+            let counterPalette = [ 255-palette[0], 255-palette[1], 255-palette[2]];
+
+            // set background and text colors
+           
+            document.body.style.backgroundColor = "rgba(" + palette[0] + "," + palette[1] + "," + palette[2] + ")";
+            fill('rgba('+ palette[0] + ',' + palette[1] + ',' + palette[2] + ',.15)');
+        }else{
+            ellipse(mouseX, mouseY, 25, 25);
+        }
     }
-  }
+}
